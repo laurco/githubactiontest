6a5427b13fae3ab0763cf6c6a75a176620780e1c
coren <corentin.lauret@outlook.fr>
2025-11-03 16:44:57 +0100

    retry

--- test_hook.py
+++ test_hook.py
@@ -38,7 +38,6 @@ class WebhookHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"OK")
         
-
 httpd = HTTPServer(('0.0.0.0', PORT), WebhookHandler)
 print(f"Listening on port {PORT}")
 httpd.serve_forever()
